48b3f71ed49d7f63140ada0e2281c4fc819c1367
cornzz <39997278+cornzz@users.noreply.github.com>
2024-11-14 19:57:00 +0100

    Fix(LLMLingua): fix prompts smaller than iterative_size not being compressed (#196)

--- llmlingua/prompt_compressor.py
+++ llmlingua/prompt_compressor.py
@@ -1413,6 +1413,8 @@ class PromptCompressor:
         self_input_ids=None,
         self_attention_mask=None,
     ):
+        if end < iterative_size:
+            end = iterative_size
         if self_loss is not None:
             need_idx = torch.concat(
                 [
